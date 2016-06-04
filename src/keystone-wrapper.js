39699c5fc535a2d45208a31df239c0e4940e4584
Alexandro Sánchez Bach <alexandro.sanchez@alumni.uni-heidelberg.de>
2016-06-04 14:32:55 +0200

    Wrapped ks_asm and implemented a demo app

--- src/keystone-wrapper.js
+++ src/keystone-wrapper.js
@@ -26,9 +26,23 @@ var ks = {
     // Modes
     MODE_LITTLE_ENDIAN:    0,  // Little-Endian mode (default mode)
     MODE_BIG_ENDIAN: 1 << 31,  // Big-Endian mode
-    MODE_X86_16:     1 <<  1,  // X86: 16-bit mode
-    MODE_X86_32:     1 <<  2,  // X86: 32-bit mode
-    MODE_X86_64:     1 <<  3,  // X86: 64-bit mode
+    MODE_ARM:        1 <<  0,  // ARM/ARM64: ARM mode
+    MODE_THUMB:      1 <<  4,  // ARM/ARM64: THUMB mode (including Thumb-2)
+    MODE_V8:         1 <<  6,  // ARM/ARM64: ARMv8 A32 encodings for ARM
+    MODE_MICRO:      1 <<  4,  // MIPS: MicroMips mode
+    MODE_MIPS3:      1 <<  5,  // MIPS: Mips III ISA
+    MODE_MIPS32R6:   1 <<  6,  // MIPS: Mips32r6 ISA
+    MODE_MIPS32:     1 <<  2,  // MIPS: Mips32 ISA
+    MODE_MIPS64:     1 <<  3,  // MIPS: Mips64 ISA
+    MODE_16:         1 <<  1,  // X86: 16-bit mode
+    MODE_32:         1 <<  2,  // X86: 32-bit mode
+    MODE_64:         1 <<  3,  // X86: 64-bit mode
+    KS_MODE_PPC32:   1 <<  2,  // PPC: 32-bit mode
+    KS_MODE_PPC64:   1 <<  3,  // PPC: 64-bit mode
+    KS_MODE_QPX:     1 <<  4,  // PPC: Quad Processing eXtensions mode
+    KS_MODE_SPARC32: 1 <<  2,  // SPARC: 32-bit mode
+    KS_MODE_SPARC64: 1 <<  3,  // SPARC: 64-bit mode
+    KS_MODE_V9:      1 <<  4,  // SPARC: SparcV9 mode
 
     /**
      * Capstone object
@@ -43,11 +57,50 @@ var ks = {
             Module._free(this.handle_ptr);
         }
 
-        // Disassemble
-        this.disasm = function (buffer, addr, max) {
-            var handle = Module.getValue(this.handle_ptr, 'i32');
+        // Assemble
+        this.asm = function (assembly) {
+            var handle = Module.getValue(this.handle_ptr, '*');
+            if (!handle) {
+                return [];
+            }
+            
+            var insn_ptr = Module._malloc(4);
+            var size_ptr = Module._malloc(4);
+            var count_ptr = Module._malloc(4);
+            
+            // Allocate buffer and copy data
+            var buffer_len = assembly.length + 1;
+            var buffer_ptr = Module._malloc(buffer_len);
+            var buffer_heap = new Uint8Array(Module.HEAPU8.buffer, buffer_ptr, buffer_len);
+            Module.writeStringToMemory(assembly, buffer_ptr);  
 
-            // TODO
+            // Run the assembler. Note that the third argument is split
+            // in the two integers that make the uint64_t address value.
+            var ret = Module.ccall('ks_asm', 'number',
+                ['pointer', 'pointer', 'number', 'number', 'pointer', 'pointer', 'pointer'],
+                [handle, buffer_heap.byteOffset, 0x0, 0x0, insn_ptr, size_ptr, count_ptr]
+            );
+            if (ret != ks.ERR_OK) {
+                return [];
+            }
+
+            // Get results
+            var insn = Module.getValue(insn_ptr, '*');
+            var size = Module.getValue(size_ptr, 'i32');
+            var count = Module.getValue(count_ptr, 'i32');
+
+            var mc = new Uint8Array(size);
+            for (var i = 0; i < size; i++) {
+                mc[i] = Module.getValue(insn + i, 'i8');
+            }
+            
+            // Free memory and return buffer
+            var ret = Module.ccall('ks_free', 'void', ['pointer'], insn_ptr);
+            Module._free(buffer_ptr);
+            Module._free(insn_ptr);
+            Module._free(size_ptr);
+            Module._free(count_ptr);
+            return mc
         };
 
         // Constructor
@@ -57,6 +110,7 @@ var ks = {
         );
 
         if (ret != ks.ERR_OK) {
+            Module.setValue(handle_ptr, 0, '*');
             console.error('Keystone.js: Function ks_open failed with code %d.', ret);
         }
     },
